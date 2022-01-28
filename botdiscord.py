d36ccf33149120556df9a90dfe811fb75bef1ba3
rezuwii <59052194+rezuwii@users.noreply.github.com>
2022-01-28 12:51:27 +0530

    Update botdiscord.py

--- botdiscord.py
+++ botdiscord.py
@@ -4,17 +4,8 @@ from discord.ext import commands
 bot = commands.Bot(command_prefix="w!", case_insensitive=True)
 
 @bot.event
-async def status_task():
-    while True:
-        await bot.change_presence(status=discord.Status.online, activity=discord.Game('Use w!help'))
-        await asyncio.sleep(50)
-        await bot.change_presence(status=discord.Status.online, activity=discord.Game('WeenPoint Admins'))
-        await asyncio.sleep(50)
-
-@bot.event
 async def on_ready():
     print('Bot is Online')
-    bot.loop.create_task(status_task())
 
 @bot.command()
 async def test(ctx):
